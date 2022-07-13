9e4154886255e042c7074fd70eabaae561df6349
Jackson Engle <https://BigPoppyJohn@github.com>
2022-07-13 09:29:19 -0500

    error validation for project identifier unique

--- src/main/java/io/jackson/ppmtool/services/ProjectService.java
+++ src/main/java/io/jackson/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.jackson.ppmtool.services;
 
 import io.jackson.ppmtool.domain.Project;
+import io.jackson.ppmtool.execptions.ProjectIdException;
 import io.jackson.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        //Logic
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch(Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
 
-        return projectRepository.save(project);
     }
 }
